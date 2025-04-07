4ed60c98e7d3b615401a0837899562c2283e6f24
DONALOMAOILCHIARAIN- <g00365724@atu.ie>
2025-04-07 11:33:53 +0100

    created test class

---
+++ src/test/java/patientTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class patientTest {
+    Patient myPat;
+
+    @BeforeEach {
+        void setup () {
+            myPat = new Patient();
+        }
+    }
+    @Test{
+        void nameSuccess(){assertEquals()
+
+    }
+
+
+}
