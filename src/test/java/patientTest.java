2db6e21ffc54ffd9c5111282c7c1231ea2d15c7c
DONALOMAOILCHIARAIN- <g00365724@atu.ie>
2025-04-07 12:18:04 +0100

    added methodh to patient class to add name

--- src/test/java/patientTest.java
+++ src/test/java/patientTest.java
@@ -2,19 +2,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class patientTest {
     Patient myPat;
 
-    @BeforeEach {
-        void setup(){
-            myPat = new Patient();
-        }
-    }
-    @Test{
-        void nameSuccess(){assertEquals()
-
+    @BeforeEach
+    void setup() {
+        myPat = new Patient();
     }
 
+    @Test
+    void nameAddSuccess() {
+        assertEquals("name added", myPat.nameAdd("john"));
+    }
+
+    @Test
+    void nameAddFail(){
+        assertThrows()
+    }
 
 }
